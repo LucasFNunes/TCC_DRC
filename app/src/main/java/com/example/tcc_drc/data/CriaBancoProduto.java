cdf86aeccdfe3a27817ff8ac52e40eaa6c4c91ec
Lucas Nunes <lucaszero12@gmail.com>
2021-11-01 16:27:18 -0300

    now working with mongo

--- app/src/main/java/com/example/tcc_drc/data/CriaBancoProduto.java
+++ app/src/main/java/com/example/tcc_drc/data/CriaBancoProduto.java
@@ -7,18 +7,15 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 import android.content.Context;
-import io.realm.Realm;
 
-public class CriaBanco extends SQLiteOpenHelper {
+public class CriaBancoProduto extends SQLiteOpenHelper {
+
     private static final String NOME_BANCO = "banco.db";
-    private static final String TABELA = "Categorias";
     private static final String TABELA1 = "Produtos";
     private static final int ID = 1;
-    private static final String NOME = "nome";
     private static final String NOME1 = "nome";
     private static final String AVALIACAO = "avaliacao";
     private static final int CATEGORIA_ID = 1;
-    private static final int VERSAO = 1;
     private static final int VALOR = 1;
     private static final String CARBO = "carboidratos";
     private static final String ACUCAR = "açucares";
@@ -30,17 +27,14 @@ public class CriaBanco extends SQLiteOpenHelper {
     private static final String SODIO = "sódio";
     private static final String FOSFORO = "fósforo";
     private static final String POTASSIO = "potassio";
+    private static final int VERSAO = 1;
 
-    public CriaBanco(Context context){
+    public CriaBancoProduto(Context context){
         super(context, NOME_BANCO,null,VERSAO);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE"+TABELA+"("
-                + ID + " integer primary key autoincrement,"
-                + NOME + " text"
-                +")";
 
         String bd = "CREATE TABLE"+TABELA1+"("
                 + ID + " integer primary key autoincrement,"
@@ -59,11 +53,12 @@ public class CriaBanco extends SQLiteOpenHelper {
                 + FOSFORO + " text,"
                 + POTASSIO + " text"
                 +")";
+        db.execSQL(bd);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS" + TABELA);
+        db.execSQL("DROP TABLE IF EXISTS" + TABELA1);
         onCreate(db);
     }
 }
--- app/src/main/java/com/example/tcc_drc/ui/activities/Bebidas.java
+++ app/src/main/java/com/example/tcc_drc/ui/activities/Bebidas.java
@@ -1,7 +1,5 @@
 package com.example.tcc_drc.ui.activities;
 
-import com.example.tcc_drc.data.CriaBanco;
-
 public class Bebidas {
 
 
